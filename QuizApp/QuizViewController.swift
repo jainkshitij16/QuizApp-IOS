0988e5e1ad91a7e2d6fdd54f26816801c86a6621
Kshitij Jain <jainkshitij16@outlook.com>
2017-01-21 17:11:40 -0800

    First Commit for Quiz App

---
+++ QuizApp/QuizViewController.swift
@@ -0,0 +1,36 @@
+//
+//  QuizViewController.swift
+//  QuizApp
+//
+//  Created by Kshitij Jain on 2017-01-21.
+//  Copyright © 2017 TryOuts. All rights reserved.
+//
+//  View controlller class for Quiz
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
